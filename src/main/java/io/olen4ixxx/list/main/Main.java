f42355a9a0c394d501a0fdf0df4e491cd8d4b208
Leanid Nasanovich <olen4ixxx@gmail.com>
2022-01-01 14:36:28 +0300

    Init

---
+++ src/main/java/io/olen4ixxx/list/main/Main.java
@@ -0,0 +1,11 @@
+package io.olen4ixxx.list.main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(1);
+        List a = new ArrayList<>();
+    }
+}
